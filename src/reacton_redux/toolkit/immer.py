02e1cbf4e62c8c1e8b5995de5b3de736c698e318
Egor Makarenko <egormkn@yandex.ru>
2024-02-15 22:48:28 +0500

    feat: add create_reducer with dummy immer implementation

---
+++ src/reacton_redux/toolkit/immer.py
@@ -0,0 +1,13 @@
+# TODO: Implement Immer functions
+
+
+def is_draftable(value):
+    return False
+
+
+def create_next_state(*args):
+    raise NotImplementedError()
+
+
+def is_draft(value):
+    return False
