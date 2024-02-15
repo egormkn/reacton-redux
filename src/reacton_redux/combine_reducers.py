3deb5b5800061b24a0f3eec046489e1c8db53ec3
Egor Makarenko <egormkn@yandex.ru>
2024-02-15 17:09:40 +0500

    fix: use SimpleNamespace in combine_reducers

--- src/reacton_redux/combine_reducers.py
+++ src/reacton_redux/combine_reducers.py
@@ -1,3 +1,4 @@
+from types import SimpleNamespace
 from typing import TypeVar, cast
 
 from .types.actions import Action
@@ -13,14 +14,14 @@ CS = TypeVar("CS")
 def combine_reducers(reducers: dict[str, Reducer[S, A]]) -> Reducer[CS, A]:
     def combination(state: CS | None, action: A) -> CS:
         if state is None:
-            state = cast(CS, object())
+            state = cast(CS, SimpleNamespace())
         has_changed = False
-        next_state = cast(CS, object())
+        next_state = cast(CS, SimpleNamespace())
         for key, reducer in reducers.items():
-            previous_state_for_key = getattr(state, key)
+            previous_state_for_key = getattr(state, key, None)
             next_state_for_key = reducer(previous_state_for_key, action)
             setattr(next_state, key, next_state_for_key)
-            has_changed = has_changed or next_state_for_key != previous_state_for_key
+            has_changed = has_changed or next_state_for_key is not previous_state_for_key
         return next_state if has_changed else state
 
     return combination
