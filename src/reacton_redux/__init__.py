768fa3396243a968825472f1073e761cc58fba94
Egor Makarenko <egormkn@yandex.ru>
2024-02-15 17:08:34 +0500

    feat: export Store from reacton_redux module

--- src/reacton_redux/__init__.py
+++ src/reacton_redux/__init__.py
@@ -1,5 +1,5 @@
 from .combine_reducers import combine_reducers
-from .create_store import create_store
+from .create_store import Store, create_store
 from .types.actions import Action
 
-__all__ = ["create_store", "combine_reducers", "Action"]
+__all__ = ["combine_reducers", "create_store", "Store", "Action"]
