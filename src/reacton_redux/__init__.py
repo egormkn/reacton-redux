af4b15b1437326cbe26372f3a8c629903c8b2df4
Egor Makarenko <egormkn@yandex.ru>
2024-02-18 13:32:31 +0500

    feat: add compose

--- src/reacton_redux/__init__.py
+++ src/reacton_redux/__init__.py
@@ -1,7 +1,8 @@
 from .combine_reducers import combine_reducers
+from .compose import compose
 from .create_store import Store, create_store
 from .types.actions import Action
 from .types.reducers import Reducer
 from .types.store import Dispatch
 
-__all__ = ["combine_reducers", "Store", "create_store", "Action", "Reducer", "Dispatch"]
+__all__ = ["combine_reducers", "compose", "Store", "create_store", "Action", "Reducer", "Dispatch"]
