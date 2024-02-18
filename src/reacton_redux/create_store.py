04e5c6366710a37ee4299a63c844e5ffe23ae33f
Egor Makarenko <egormkn@yandex.ru>
2024-02-18 15:29:08 +0500

    feat: store enhancers and middleware support

--- src/reacton_redux/create_store.py
+++ src/reacton_redux/create_store.py
@@ -4,21 +4,22 @@ from typing import Any, Callable, Generic, TypeVar, cast
 
 from .types.actions import Action
 from .types.reducers import Reducer
-from .types.store import ListenerCallback
+from .types.store import ListenerCallback, Store, StoreEnhancer
+from .utils.action_types import ActionTypes
 
 S = TypeVar("S")
 
 A = TypeVar("A", bound=Action[Any, Any, Any])
 
 
-class Store(Generic[S, A]):
+class StoreImpl(Generic[S, A]):
     def __init__(self, reducer: Reducer[S, A], preloaded_state: S | None = None) -> None:
         self.reducer = reducer
         self.state: S = cast(S, preloaded_state)
         self.listeners: set[ListenerCallback] = set()
         self.is_dispatching = False
 
-        self.dispatch({"type": "@@INIT"})  # type: ignore[arg-type]
+        self.dispatch = self._dispatch
 
     def get_state(self) -> S:
         if self.is_dispatching:
@@ -39,7 +40,7 @@ class Store(Generic[S, A]):
 
         return unsubscribe
 
-    def dispatch(self, action: A) -> A:
+    def _dispatch(self, action: A) -> A:
         if self.is_dispatching:
             raise Exception("Reducers may not dispatch actions")
 
@@ -55,4 +56,21 @@ class Store(Generic[S, A]):
         return action
 
 
-create_store = Store
+def create_store(
+    reducer: Reducer[S, A],
+    preloaded_state: S | None = None,
+    enhancer: StoreEnhancer | None = None,
+):
+    if not callable(reducer):
+        raise ValueError(f"Expected the root reducer to be a function. Instead, received: '{type(reducer)}'")
+
+    if enhancer is not None:
+        if not callable(enhancer):
+            raise ValueError(f"Expected the enhancer to be a function. Instead, received: '{type(enhancer)}'")
+
+        return enhancer(create_store)(reducer, preloaded_state)
+
+    store: Store[S, A] = StoreImpl(reducer, preloaded_state)
+
+    store.dispatch({"type": ActionTypes.INIT})  # type: ignore[arg-type]
+    return store
--- src/reacton_redux/types/store.py
+++ src/reacton_redux/types/store.py
@@ -1,6 +1,11 @@
+from __future__ import annotations
+
 from typing import Any, Callable, Protocol, TypeAlias, TypeVar
 
 from .actions import Action
+from .reducers import Reducer
+
+S = TypeVar("S")
 
 A = TypeVar("A", bound=Action[Any, Any, Any])
 
@@ -10,3 +15,20 @@ class Dispatch(Protocol[A]):
 
 
 ListenerCallback: TypeAlias = Callable[[], None]
+
+Unsubscribe: TypeAlias = Callable[[], None]
+
+
+class Store(Protocol[S, A]):  # type: ignore
+    dispatch: Dispatch[A]
+
+    def get_state(self) -> S: ...
+
+    def subscribe(self, listener: ListenerCallback) -> Unsubscribe: ...
+
+
+class StoreEnhancerStoreCreator(Protocol[S, A]):
+    def __call__(self, reducer: Reducer[S, A], preloaded_state: S | None) -> Store[S, A]: ...
+
+
+StoreEnhancer = Callable[[StoreEnhancerStoreCreator], StoreEnhancerStoreCreator]
