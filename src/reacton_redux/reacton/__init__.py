b6fea87df30a7b295c5261b727f169a3ff1e2c5d
Egor Makarenko <egormkn@yandex.ru>
2024-02-19 17:37:36 +0500

    fix: do not export devtools from `reacton_redux.reacton`

--- src/reacton_redux/reacton/__init__.py
+++ src/reacton_redux/reacton/__init__.py
@@ -1,4 +1,3 @@
-from .devtools import StoreState
 from .reacton import StoreProvider, use_dispatch, use_selector, use_store
 
-__all__ = ["StoreState", "StoreProvider", "use_store", "use_dispatch", "use_selector"]
+__all__ = ["StoreProvider", "use_store", "use_dispatch", "use_selector"]
